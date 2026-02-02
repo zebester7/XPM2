1e5c973f12a8dbc22e83f4db6e08910b13873da8
Zubair Ahmad <zubairahmadisb7@gmail.com>
2026-02-02 22:30:34 +0500

    Update routing and SEO setup in index.tsx

--- index.tsx
+++ index.tsx
@@ -1,24 +1,16 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
-import { HelmetProvider } from 'react-helmet-async';
-
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
-  throw new Error('Could not find root element to mount to');
+  throw new Error("Could not find root element to mount to");
 }
 
 const root = ReactDOM.createRoot(rootElement);
-
 root.render(
   <React.StrictMode>
-    <HelmetProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </HelmetProvider>
+    <App />
   </React.StrictMode>
 );
-
